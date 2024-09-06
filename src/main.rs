9bbda5d4e8ca10443286573a06c9472338acf821
Petar <petar@andrejic.science>
2024-09-06 22:35:14 +1200

    Make inner code async

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{error::Error, sync::Arc, time::Duration};
 
 use webgpu_compute::Engine;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let engine = Arc::new(Engine::new().block_on()?);
+async fn run(engine: Arc<Engine>) -> Result<(), Box<dyn Error>> {
     let shader_desc = wgpu::include_wgsl!("../shaders/hello_world.wgsl");
     let module = engine.device.create_shader_module(shader_desc);
     let op = engine.create_operation(&module, "hello world".to_string());
@@ -12,20 +11,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     let tmp = engine.create_gpu_vec::<u32>(4);
     let mut data_out = engine.create_gpu_vec::<u32>(4);
 
-    let (poll_join, mut poll_close) = engine.clone().create_poll_loop(Duration::from_millis(100));
-    engine.to_gpu(&data_in).block_on()?;
+    engine.to_gpu(&data_in).await?;
 
     let view1 = [data_in.buf(), tmp.buf()];
     let view2 = [tmp.buf(), data_out.buf()];
 
-    engine.dispatch([(&op, view1), (&op, view2)]).block_on()?;
+    engine.dispatch([(&op, view1), (&op, view2)]).await?;
 
-    engine.to_cpu(&mut data_out).block_on()?;
+    engine.to_cpu(&mut data_out).await?;
     print!("[ ");
     for i in data_out.data {
         print!("{} ", i);
     }
     println!("]");
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let engine = Arc::new(Engine::new().block_on()?);
+    let (poll_join, mut poll_close) = engine.clone().create_poll_loop(Duration::from_millis(100));
+    run(engine).block_on()?;
 
     poll_close.close();
     poll_join.join().expect("Failed to join poll loop");
